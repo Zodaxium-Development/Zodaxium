07d1cc1707e917e34b76a9d8e336b0da18aadac5
TheInfoCoder <dylanmcpeek12@gmail.com>
2014-05-24 01:18:06 -0500

    Switch to a nicer gamemode test

--- src/me/zodaxium/zessentials/commands/Commandgamemode.java
+++ src/me/zodaxium/zessentials/commands/Commandgamemode.java
@@ -25,36 +25,44 @@ public class Commandgamemode implements CommandExecutor{
 			Player p = (Player) sender;
 			if(p.hasPermission(Reference.PERM_ADMIN)){
 				if(args.length == 1){
-					if(args[0].equals("0")){
-						p.setGameMode(GameMode.SURVIVAL);
-						p.sendMessage(Reference.colorize(Reference.PREFIX + "&aGameMode: &9&osurvival"));
-					}else if(args[0].equals("1")){
-						p.setGameMode(GameMode.CREATIVE);
-						p.sendMessage(Reference.colorize(Reference.PREFIX + "&aGameMode: &9&ocreative"));
-					}else if(args[0].equals("2")){
-						p.setGameMode(GameMode.ADVENTURE);
-						p.sendMessage(Reference.colorize(Reference.PREFIX + "&aGameMode: &9&oadventure"));
-					}else{
-						p.sendMessage(Reference.colorize(Reference.PREFIX + "&aUsage: /Gm (Mode)"));
+					switch(args[0]){
+						case "0":
+							p.setGameMode(GameMode.SURVIVAL);
+							p.sendMessage(Reference.colorize(Reference.PREFIX + "&aGameMode: &9&osurvival"));
+							break;
+						case "1":
+							p.setGameMode(GameMode.CREATIVE);
+							p.sendMessage(Reference.colorize(Reference.PREFIX + "&aGameMode: &9&ocreative"));
+							break;
+						case "2":
+							p.setGameMode(GameMode.ADVENTURE);
+							p.sendMessage(Reference.colorize(Reference.PREFIX + "&aGameMode: &9&oadventure"));
+							break;
+						default:
+							p.sendMessage(Reference.colorize(Reference.PREFIX + "&aUsage: /Gm (Mode)"));
 					}
 				}else if(!(args.length < 2)){
 					Player t = plugin.getServer().getPlayer(args[1]);
 					if(p.hasPermission(Reference.PERM_ADMIN)){
 						if(t != null){
-							if(args[0].equals("0")){
-								t.setGameMode(GameMode.SURVIVAL);
-								t.sendMessage(Reference.colorize(Reference.PREFIX + "&aGameMode: &9&osurvival"));
-								p.sendMessage(Reference.colorize(Reference.PREFIX + "&a" + t.getName() + " GameMode: &9&osurvival"));
-							}else if(args[0].equals("1")){
-								t.setGameMode(GameMode.CREATIVE);
-								t.sendMessage(Reference.colorize(Reference.PREFIX + "&aGameMode: &9&ocreative"));
-								p.sendMessage(Reference.colorize(Reference.PREFIX + "&a" + t.getName() + " GameMode: &9&ocreative"));
-							}else if(args[0].equals("2")){
-								t.setGameMode(GameMode.ADVENTURE);
-								t.sendMessage(Reference.colorize(Reference.PREFIX + "&aGameMode: &9&oadventure"));
-								p.sendMessage(Reference.colorize(Reference.PREFIX + "&a" + t.getName() + " GameMode: &9&oadventure"));
-							}else{
-								p.sendMessage(Reference.colorize(Reference.PREFIX + "&aUsage: /gm (mode)"));
+							switch(args[0]){
+								case "0":
+									t.setGameMode(GameMode.SURVIVAL);
+									t.sendMessage(Reference.colorize(Reference.PREFIX + "&aGameMode: &9&osurvival"));
+									p.sendMessage(Reference.colorize(Reference.PREFIX + "&a" + t.getName() + " GameMode: &9&osurvival"));
+									break;
+								case "1":
+									t.setGameMode(GameMode.CREATIVE);
+									t.sendMessage(Reference.colorize(Reference.PREFIX + "&aGameMode: &9&ocreative"));
+									p.sendMessage(Reference.colorize(Reference.PREFIX + "&a" + t.getName() + " GameMode: &9&ocreative"));
+									break;
+								case "2":
+									t.setGameMode(GameMode.ADVENTURE);
+									t.sendMessage(Reference.colorize(Reference.PREFIX + "&aGameMode: &9&oadventure"));
+									p.sendMessage(Reference.colorize(Reference.PREFIX + "&a" + t.getName() + " GameMode: &9&oadventure"));
+									break;
+								default:
+									p.sendMessage(Reference.colorize(Reference.PREFIX + "&aUsage: /Gm (Mode) (Player)"));
 							}
 						}else{
 							p.sendMessage(Reference.DENY_USER);
